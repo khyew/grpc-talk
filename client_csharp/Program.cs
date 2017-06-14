67d5e307c1b64572d7c0bde2021c341317737328
Qifan Xi <qifan@vreal.io>
2017-06-14 15:18:30 -0700

    Added package name for protobuf file
    Necessary to generate working C# code

--- client_csharp/Program.cs
+++ client_csharp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
+using Demo;
 
 namespace client
 {
@@ -10,6 +12,26 @@ namespace client
     {
         static void Main(string[] args)
         {
+            String[] arguments = Environment.GetCommandLineArgs();
+
+            if (arguments.Length != 4) {
+                Console.WriteLine("Usage: {0} IP PORT NAME", arguments[0]);
+                return;
+            }
+
+            var ip = arguments[1];
+            var port = arguments[2];
+            var name = arguments[3];
+
+            Channel channel = new Channel(ip + ":" + port, ChannelCredentials.Insecure);
+            var client = new DemoService.DemoServiceClient(channel);
+
+            var reply = client.Hello(new HelloRequest
+            {
+                Name = name
+            });
+
+            Console.WriteLine(reply.Reply);
         }
     }
 }
